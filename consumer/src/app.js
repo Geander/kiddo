1b1b2ce12955ea485bd3c7a907c30fd3e8e44f03
Geander Rodrigues <geander@gmail.com>
2019-01-20 21:51:59 -0200

    refactor app

--- consumer/src/app.js
+++ consumer/src/app.js
@@ -7,26 +7,13 @@ db.on('error', err => {
   process.exit(1);
 });
 
-amqp.then(conn => {
-  return conn.createChannel();
-}).then(ch => {
+amqp.then(conn => conn.createChannel()).then(ch => {
   const q = 'languages';
   return ch.assertQueue(q, { durable: false }).then(() => {
-    console.log(' [*] Waiting for messages in %s.', q);
+    console.log('Waiting for messages in %s.', q);
     return ch.consume(q, msg => {
-      if (msg !== null) {
-        console.log(' [x] Received %s', msg.content.toString());
-        const msgParsed = JSON.parse(msg.content);
-        let languages = new Languages({
-          'name': msgParsed.name,
-          'wikipedia_url': msgParsed.wikipedia_url
-        });
-        languages.save(err => {
-          if (err) {
-            console.error(err);
-            process.exit(1);
-          }
-        });
+      if (msg) {
+        save(msg);
         ch.ack(msg);
       }
     });
@@ -35,3 +22,18 @@ amqp.then(conn => {
   console.error(err);
   process.exit(1);
 });
+
+const save = msg => {
+  console.log('Received %s', msg.content.toString());
+  const msgParsed = JSON.parse(msg.content);
+  let languages = new Languages({
+    'name': msgParsed.name,
+    'wikipedia_url': msgParsed.wikipedia_url
+  });
+  languages.save(err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+};
