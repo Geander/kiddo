5cfe4dd5c4d96e5face1155247a863848af13aa1
Geander Rodrigues <geander@gmail.com>
2019-01-20 01:11:00 -0200

    changed to promisse

--- consumer/src/app.js
+++ consumer/src/app.js
@@ -1,5 +1,4 @@
-const config = require('../config');
-const amqp = require('amqplib/callback_api');
+const amqp = require('./amqp');
 const Languages = require('./models/languages');
 const db = require('./database');
 
@@ -8,42 +7,31 @@ db.on('error', err => {
   process.exit(1);
 });
 
-amqp.connect(`amqp://${config.amqp.uri}:${config.amqp.port}`, (err, conn) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  } else {
-    conn.createChannel((err, ch) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      } else {
-        const q = 'languages';
-
-        ch.assertQueue(q, { durable: false });
-        console.log(' [*] Waiting for messages in %s.', q);
-        ch.consume(q, msg => {
-          console.log(' [x] Received %s', msg.content.toString());
-          const msgParsed = JSON.parse(msg.content);
-
-          let languages = new Languages(
-            {
-              'name': msgParsed.name,
-              'wikipedia_url': msgParsed.wikipedia_url
-            }
-          );
-
-          languages.save(err => {
-            if (err) {
-              console.error(err);
-              process.exit(1);
-            }
-          });
-
-        }, { noAck: true });
-
+amqp.then(conn => {
+  return conn.createChannel();
+}).then(ch => {
+  const q = 'languages';
+  return ch.assertQueue(q, { durable: false }).then(() => {
+    console.log(' [*] Waiting for messages in %s.', q);
+    return ch.consume(q, msg => {
+      if (msg !== null) {
+        console.log(' [x] Received %s', msg.content.toString());
+        const msgParsed = JSON.parse(msg.content);
+        let languages = new Languages({
+          'name': msgParsed.name,
+          'wikipedia_url': msgParsed.wikipedia_url
+        });
+        languages.save(err => {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
+        });
+        ch.ack(msg);
       }
-
     });
-  }
+  });
+}).catch(err => {
+  console.error(err);
+  process.exit(1);
 });
